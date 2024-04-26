f919e44602a9d324a6479858c5575008b823deef
Exelupra <101408547+Exelupra@users.noreply.github.com>
2024-04-26 21:41:29 +0200

    Test POur profil

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <header class="head">
       <NuxtLink to="/" class="button">Accueil </NuxtLink>
       <NuxtLink to="/forum-liste" class="button">Forum </NuxtLink>
-      <NuxtLink to="/" class="button" v-if="connected">Profil </NuxtLink>
+      <NuxtLink to="/Profil" class="button" v-if="connected">Profil </NuxtLink>
       <NuxtLink to="Connexion" class="button" v-if="!connected">Connexion </NuxtLink>
       <NuxtLink to="Inscription" class="button" v-if="!connected">Inscription</NuxtLink>
       <NuxtLink to="/" class="button" v-if="connected">Deconnexion </NuxtLink>
@@ -26,6 +26,8 @@ data() {
   mounted() {
   this.connected=true;
   this.user=1;
+  sessionStorage.setItem('user',null);
+
   },
   methods: {
     swap() {
---
+++ pages/Profil.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="profil-container">
+    <h1>Profil</h1>
+    <div v-if="user">
+      <p>Nom d'utilisateur : {{ user.Name }}</p>
+      <p>Rôle : {{ user.Admin === 1? 'Administrateur' : 'Utilisateur' }}</p>
+    </div>
+    <div v-else>
+      <p>Aucun utilisateur connecté</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+
+    return {
+      user
+    };
+  }
+}
+</script>
+
+<style>
+.profil-container {
+  background-color: #f0f0f0;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.profil-container h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+.profil-container p {
+  font-size: 1.2rem;
+  color: #333;
+  margin-bottom: 10px;
+}
+</style>
